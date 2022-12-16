3d75d6835bd4204338601b4c78e24186fe685c73
Lautagonzalez9 <lauta_gonzalez29@hotmail.com>
2022-12-16 12:26:42 -0300

    Levantamos el servidor, cree la ruta de home y de carrito, tambien cree un archivo css para lso styles necesarios para carrito

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 3500;
+const path = require('path');
+const publicPath = path.join(__dirname, '/public');
+//uso estatico
+app.use(express.static(publicPath));
+//levantar servidor
+app.listen (3500, ()=>{console.log('Servidor con puerto 3500 corriendo')});
+//Rutas y links
+app.get('localhost:3500/',(req,res)=>{res.sendFile(path.join(__dirname,'./views/home.html'))});
+app.get('/',(req,res)=>{res.sendFile(path.join(__dirname,'./views/home.html'))});
+app.get('/carrito',(req,res)=>{res.sendFile(path.join(__dirname,'./views/carrito.html'))})
