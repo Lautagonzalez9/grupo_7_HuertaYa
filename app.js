80456e12f4767e17f67cd158f42712f198ca9740
ocean-girl <vpdamill@gmail.com>
2022-12-22 19:54:47 -0300

    modifique en app.js, la ruta del home por index.html

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(express.static(publicPath));
 //levantar servidor
 app.listen (3500, ()=>{console.log('Servidor con puerto 3500 corriendo')});
 //Rutas y links
-app.get('localhost:3500/',(req,res)=>{res.sendFile(path.join(__dirname,'./views/home.html'))});
-app.get('/',(req,res)=>{res.sendFile(path.join(__dirname,'./views/home.html'))});
+app.get('localhost:3500/',(req,res)=>{res.sendFile(path.join(__dirname,'./views/index.html'))});
+app.get('/',(req,res)=>{res.sendFile(path.join(__dirname,'./views/index.html'))});
 app.get('/carrito',(req,res)=>{res.sendFile(path.join(__dirname,'./views/carrito.html'))})
 app.get("/detalle-de-producto",(req,res)=>{res.sendFile(path.join(__dirname,"./views/detalle-del-producto.html"))});
