945c6357c9a14ce929c18f6624d754d9d380e5fc
PennaJ <julianpenna96@gmail.com>
2023-01-20 19:18:45 -0300

    se agregó una ruta para editar el producto

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -4,6 +4,9 @@ const path = require('path');
 const adminController = {
     create: function(req,res){
         res.render('products/adminCreate')
+    },
+    edit: function(req,res){
+        res.render('products/editarProducto')
     }
 }
 
--- routes/admin.js
+++ routes/admin.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const adminController = require('../controllers/adminController')
 
 router.get('/create', adminController.create)
+router.get('/editar', adminController.edit)
 
 
 module.exports = router;
