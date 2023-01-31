5eadb553c5a709aefb36b3e866aeffbcb8c8c273
PennaJ <julianpenna96@gmail.com>
2023-01-31 20:27:07 -0300

    agregué la función de búsqueda y la vista resultados de búsqueda

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,10 +1,20 @@
 const path = require('path');
+const fs = require('fs');
+
+const productsFilePath = path.join(__dirname, "../data/productsData.json");
+const productos = JSON.parse(fs.readFileSync(productsFilePath, "utf-8"));
 
 const mainController = {
     index : function(req,res){
         res.render('index')
        
     },
+    search: (req, res) => {
+		const search = req.query.keywords;
+		const product = [];
+        product.push(productos.find(product => product.name.toLowerCase().indexOf(search.toLowerCase()) >= 0));
+		return res.render("./products/searchResult", { productos: product })
+	},
     carrito: function(req,res){
         res.render('./products/carrito')
         
