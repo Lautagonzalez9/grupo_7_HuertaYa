c26a2d7e0b8ae05ad3ff8ac8824eb6ae210aa84f
ocean-girl <vpdamill@gmail.com>
2023-02-02 12:18:02 -0300

    muevo productsData.json a carpeta data anterior, actualice rutas

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const fs = require('fs');
 
-const productsFilePath = path.join(__dirname, "../data/productsData.json");
+const productsFilePath = path.join(__dirname, "./data/productsData.json");
 const productos = JSON.parse(fs.readFileSync(productsFilePath, "utf-8"));
 
 const mainController = {
