848e8207a009d35b8cba90e3348905f433e06477
ocean-girl <vpdamill@gmail.com>
2023-02-08 20:08:28 -0300

    agrego ruta y controlador para edición de productos, modifico vista de edición y agregué botón de edición en el listado de productos

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -8,12 +8,13 @@ const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
 const adminController = {
     create: function(req,res){
-        res.render('products/adminCreate')
+        res.render('products/adminCreate');
     },
     creado: function(req,res){
         
         let data = fs.readFileSync(path.join(__dirname, './data/productsData.json'), {encoding : 'utf-8'});
-        const product ={
+        
+        const product ={ 
             id: products.length > 0 ? products[products.length -1].id + 1 : 1,
             name: req.body.Name,
             descripcion: req.body.descripcion,
@@ -37,14 +38,39 @@ const adminController = {
         res.redirect('/');
        },
 
-
-
-    edit: function(req,res){
-        res.render('products/editarProducto')
-    },
-    editado: function(req,res){
-        
-    }
+       fileProducts: './data/productsData.json',
+    
+       dataProducts: function(){
+           return JSON.parse(fs.readFileSync(this.fileProducts,'utf-8'));
+       },
+          
+       findAllProducts: function (){
+           return this.dataProducts();
+       },
+   
+       findProductsId: function(id){
+           let allProducts = this.findAllProducts();
+           let idFound = allProducts.find(oneProduct => oneProduct.id === id);
+           return idFound
+       },
+       
+       edit: function(req,res){
+           const id = req.params.id
+           let allProducts = adminController.findAllProducts();
+           let editProducts = allProducts.find(oneProduct => oneProduct.id === id);
+           return res.render("./products/editarProducto", { editProducts });
+       },
+   
+       edited: function(req,res){
+           return res.render("Producto editado");
+       },
+   
+       delete: function(id){
+           let allProducts = this.findAllProducts();
+           let finalProducts = allProducts.filter(oneProduct => oneProduct.id !== id);
+           fs.writeFileSync(this.fileProducts, JSON.stringify(finalProducts, null, 2));
+           return true
+       }
 }
 
 
