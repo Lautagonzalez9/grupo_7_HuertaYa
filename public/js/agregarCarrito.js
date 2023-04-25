ac621d9cf741115c2a921d570a8f055ce1d5d99c
MarcoSavarin0 <ateneo2003@gmail.com>
2023-04-24 23:43:49 -0300

    funcion de agregar al carrito en funcion

---
+++ public/js/agregarCarrito.js
@@ -0,0 +1,32 @@
+
+let botonesAgregarCarrito = document.querySelectorAll('.agregar-carrito');
+
+let carrito = [];
+
+
+function agregarAlCarrito(evento) {
+  let button = evento.target;
+  let item = button.closest('.producto');
+  let itemTitle = item.querySelector('.producto-detalle h3').textContent;
+  let itemPrice = item.querySelector('.precio').textContent;
+  let itemImage = item.querySelector('.imagen-producto').src;
+
+  let newItem = {
+    title: itemTitle,
+    price: itemPrice,
+    image: itemImage,
+  };
+
+  carrito.push(newItem);
+  localStorage.setItem('carrito', JSON.stringify(carrito));
+  console.log(newItem);
+}
+
+for (let i = 0; i < botonesAgregarCarrito.length; i++) {
+  let button = botonesAgregarCarrito[i];
+  button.addEventListener('click', agregarAlCarrito);
+  
+}
+  
+
+console.log(JSON.parse(localStorage.getItem('carrito')));
---
+++ public/js/carrito.js
@@ -0,0 +1,63 @@
+let carrito = JSON.parse(localStorage.getItem('carrito')) || [];
+console.log(carrito);
+let precio = document.querySelector('.price')
+let nombreProd = document.querySelector('.producto')
+let imagen = document.querySelector('.imagen')
+const main = document.querySelector(".conteiner")  
+
+if (carrito.length > 0) {
+    if (carrito.length <= 2) {
+        const primerProducto = carrito[0];
+        const precio = document.querySelector('.price');
+        const nombreProd = document.querySelector('.producto');
+
+        precio.innerHTML = primerProducto.price;
+        nombreProd.innerHTML = primerProducto.title;
+        
+    } else {
+        const main = document.querySelector(".conteiner");
+        carrito.forEach(product => {
+            const productItem = document.createElement('div');
+            productItem.classList.add('contenedor-articulos');
+            productItem.innerHTML = `
+                <img src="${product.image}" alt="${product.title}">
+                <p class="producto">${product.title}</p>
+                <p class="price">${product.price}</p>
+                <form action="#">
+                    <select name="" id="" class="cantidad">
+                        <option value="">1</option>
+                        <option value="">2</option>
+                        <option value="">3</option>
+                        <option value="">4</option>
+                        <option value="">5</option>
+                        <option value="">6</option>
+                    </select>                                              
+                </form>
+                <a href="#" class="delete"><i class="fa-solid fa-trash"></i></a>
+            `;
+            main.appendChild(productItem);   
+        });
+    }
+}
+
+function eliminarProducto(event) {
+    
+    const botonEliminar = event.target;
+    const indice = parseInt(botonEliminar.dataset.indice);
+  
+    carrito.splice(indice, 1);
+  
+    
+    localStorage.setItem('carrito', JSON.stringify(carrito));
+  
+   
+    const productoEliminar = botonEliminar.parentNode.parentNode;
+    productoEliminar.remove();
+  }
+  
+ 
+  const botonesEliminar = document.querySelectorAll('.delete');
+  botonesEliminar.forEach((boton, indice) => {
+    boton.dataset.indice = indice;
+    boton.addEventListener('click', eliminarProducto);
+  });
