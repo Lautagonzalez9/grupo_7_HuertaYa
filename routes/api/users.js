4edf6ca300fc812048216eac8068b4b5eb01152a
ocean-girl <vpdamill@gmail.com>
2023-04-30 11:27:54 -0300

    subo método list en router/api/users.js

---
+++ routes/api/users.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const path = require('path');
+const router = express.Router(); 
+const controllerUserApi = require('../../controllers/api/controllerUserApi');
+
+//Listado de usuarios
+router.get('/', controllerUserApi.list);
