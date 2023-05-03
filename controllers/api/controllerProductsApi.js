5e56b467802634cdfe9074b6a7e0f1a03f3e7af4
PennaJ <julianpenna96@gmail.com>
2023-05-03 20:00:19 -0300

    controller api andando a medias

--- controllers/api/controllerProductsApi.js
+++ controllers/api/controllerProductsApi.js
@@ -24,32 +24,67 @@ module.exports = {
         res.json(respuesta)
         })
     },
-    listProducts: function(req,res){    
-        db.products.findAll({
-            include:[
-                {association: "images"},
-                {association: "categories"},
-                {association: "presentations"}
-            ]
-        }).then(products => {
-            let respuesta = {
-                meta:{
-                    status: {
-                        status: 200,
-                        url: '/api/products'
-                    },
-                    //data: {
-                    //    count: products.length,
-                    //    countByCategory:
-                        //    db.category.
-                        //     products.categories.map(category => {
-                        //         console.log(category)
-                        // }
-                        // Revisar en la base de datos bien como llamar la categoria
-                    //}
-                } 
-            }
-            res.json(respuesta)
+    listProducts: function(req,res){
+           
+        const countByCategory = {}
+
+            db.categories.findAll()
+            .then(categories => {
+            categories.forEach(category => {
+
+                let categoria = category.name
+
+                    db.products.findAll({
+                    where: {
+                        id_category: category.idcategory
+                    }
+                })
+                .then(products => {
+                    products.length
+                })
+                .then(numeroProds => {
+                    countByCategory[categoria] = numeroProds
+                })
             })
-    }
+                
+                db.products.findAll({
+                    include:[
+                        {association: "images"},
+                        {association: "categories"},
+                        {association: "presentations"}
+                    ]
+                }).then(products => {
+        
+                    let productos = []
+                    
+                    products.forEach(product => {
+                        productos.push({
+                            id: product.idProducto,
+                            name: product.name,
+                            description: product.description,
+                            detail: "/producto/" + product.idProducto
+        
+                    })
+                    })
+        
+                    let respuesta = {
+                        meta:{
+                            status: {
+                                status: 200,
+                                url: '/api/products'
+                            },
+                            data: {
+                                count: products.length,
+                                countByCategory: countByCategory,
+                                products: productos
+                            }
+                        } 
+                        
+                    }
+                    res.json(respuesta)
+                
+            })
+    
+        })
+        }
 }
