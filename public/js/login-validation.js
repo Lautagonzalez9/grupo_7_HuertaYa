810a58f035d2cadbaef2828e4bfc98d4149ba8c2
manuel-chirino <manuelchg.92@gmail.com>
2023-05-03 20:22:28 -0400

    se modifica validacion del login,quedo validando front y back

--- public/js/login-validation.js
+++ public/js/login-validation.js
@@ -1,55 +1,45 @@
 window.onload=function(){
-   const db = require('../src/database/models');
-   const User = require('../models/User')
-   const bcryptjs = require("bcryptjs");  
+
    const capturaFormulario=document.querySelector('.formulario');
    console.log(capturaFormulario)
 
-   capturaFormulario.addEventListener("submit", async (event) => { 
+   capturaFormulario.addEventListener("submit",(event) => { 
       event.preventDefault();
         let correoElectronico=document.querySelector('input.correo');
-        let contraseñaLogin=document.querySelector('.contraseña')
+        let contraseñaLogin=document.querySelector('input.contraseña')
 
-        // validacion con alert en pantalla
-        // if(correoElectronico.value == ""){
-        //   alert("Debe ingresar correo electronico para iniciar sesión")
-        // };
-        // if(contraseñaLogin.value == ""){
-        //     alert("Ingrese contraseña para iniciar sesión")
-        // }
+        //validacion con alert en pantalla
+      //   if(correoElectronico.value == ""){
+      //     alert("Debe ingresar correo electronico para iniciar sesión")
+      //   };
+      //   if(contraseñaLogin.value == ""){
+      //       alert("Ingrese contraseña para iniciar sesión")
+      //   }
         
-        // validacion con ciclo for y array de errores
+       // validacion con ciclo for y array de errores
          let erroresLogin= [];
         
          if (correoElectronico.value == ""){
             erroresLogin.push("Debe ingresar correo electronico para iniciar sesión")
          };
-         db.user.findAll({
-            where:{
-              email: req.body.email
-            }
-          }).then(users =>{if(users.length > 0){
-            const user = users[0];
-            console.log(user.password);  
-            if(! bcryptjs.compare(req.body.password, user.password)){
-               res.render('./users/login.ejs',{
-                  errors: {
-                        contrasenia: {
-                         msg: 'La contraseña es incorrecta'
-                         }
-         }})
-         }}
-         let ulErroresLogin=document.querySelector("div .erroresLogin ul");
+         if (contraseñaLogin.value == ""){
+            erroresLogin.push("Ingrese contraseña para iniciar sessión")
+         };
+       
+        let ulErroresLogin=document.querySelector("div .erroresLogin ul");
 
          if(erroresLogin.length >0){
-            event.preventDefault();
+           // event.preventDefault();
             
             ulErroresLogin.innerHTML=""
             for (let error of erroresLogin) {
                 ulErroresLogin.innerHTML += "<li>" + error + "</li>"
             }
         
+         }else{
+         
+            capturaFormulario.submit();
          }
     })
-   })
-}
+    }
+
