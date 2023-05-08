001dd54868f08964909a2a748c699f69110780f4
ocean-girl <vpdamill@gmail.com>
2023-05-08 16:07:25 -0300

    actualizo controllerUserApi con el método listado de usuarios terminado, agregue detail y modifiqué los datos que mostraba

--- controllers/api/controllerUserApi.js
+++ controllers/api/controllerUserApi.js
@@ -8,6 +8,19 @@ const usersApiController ={
         //return res.send('prueba api')
         db.user.findAll()
         .then(users => {
+                    
+            // Creo un arreglo con los objetos que solo contienen nombre y apellido
+            const personas = users.map(result => {
+              return {
+                iduser: result.iduser,
+                first_name: result.first_name,
+                last_name: result.last_name,
+                email: result.email,
+                detail: "/users/" + result.iduser
+        
+              };
+            });
+          
             //return res.send(users)
             let respuesta = {
                 meta: {
@@ -15,8 +28,9 @@ const usersApiController ={
                     count: users.length,
                     url: '/api/users'
                 },
-                data: users                    
-            }
+                data: personas
+                }
+            
             res.json(respuesta)
         })
     },
